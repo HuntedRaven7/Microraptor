8b8b0aea646d2dc0131514b2358ecfe80b2bf185
Robin Groves <EdwardGroves2007@proton.me>
2026-09-07 13:42:00 -0400

    fix: fsdk-it/tailscale.bst runs download-tailscale.sh, which calls wget. wget is not present in the FSDK build sandbox.

--- files/os/tailscale/download-tailscale.sh
+++ files/os/tailscale/download-tailscale.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 URL="${TAILSCALE_URL}"
-wget -q -O /tmp/tailscale.tgz "${URL}"
+curl -fsSL -o /tmp/tailscale.tgz "${URL}"
 tar -xzf /tmp/tailscale.tgz -C /tmp
 chmod 0755 /tmp/tailscale /tmp/tailscaled
