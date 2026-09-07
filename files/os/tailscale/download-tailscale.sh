a151a7c94c257e77b4a19c28da57778088d9d9e9
Robin Groves <EdwardGroves2007@proton.me>
2026-09-07 13:55:52 -0400

    feat: added curl

--- files/os/tailscale/download-tailscale.sh
+++ files/os/tailscale/download-tailscale.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -eu
 URL="${TAILSCALE_URL}"
-python3 - "$URL" <<'PY'
-import sys, urllib.request
-url = sys.argv[1]
-with urllib.request.urlopen(url) as r, open('/tmp/tailscale.tgz','wb') as f:
-    f.write(r.read())
-PY
+curl -fsSL -o /tmp/tailscale.tgz "${URL}"
 tar -xzf /tmp/tailscale.tgz -C /tmp
 chmod 0755 /tmp/tailscale /tmp/tailscaled
