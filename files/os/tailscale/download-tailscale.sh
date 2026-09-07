0e466a421b6c402b04fe3acff29091fce7ea429e
Robin Groves <EdwardGroves2007@proton.me>
2026-09-07 13:48:50 -0400

    fix: curl tmp

--- files/os/tailscale/download-tailscale.sh
+++ files/os/tailscale/download-tailscale.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -eu
 URL="${TAILSCALE_URL}"
-curl -fsSL -o /tmp/tailscale.tgz "${URL}"
+python3 - "$URL" <<'PY'
+import sys, urllib.request
+url = sys.argv[1]
+with urllib.request.urlopen(url) as r, open('/tmp/tailscale.tgz','wb') as f:
+    f.write(r.read())
+PY
 tar -xzf /tmp/tailscale.tgz -C /tmp
 chmod 0755 /tmp/tailscale /tmp/tailscaled
